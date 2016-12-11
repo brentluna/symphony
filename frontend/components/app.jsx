2f8718219a2b24e8391327ab5ee55f41d7525809
brentluna <brentadamluna@gmail.com>
2016-12-11 13:54:26 -0800

    refactored the root comonent to a root and app component and added react trouter to the root component, making the ItemList   the Index Route. Now I need to create a individual product page and create a route for those

---
+++ frontend/components/app.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {fetchProducts} from '../actions/product_actions';
+import ProductItemListContainer from './products/product_item_list_container';
+import NavBarContainer from './navbar/navbar_container';
+require('../../sass/reset.scss');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleClass = this.toggleClass.bind(this);
+  }
+
+  
+  toggleClass(e) {
+    let cart = document.getElementById('cart-ul');
+    let sortBy = document.getElementById('sort-by-ul');
+    let filter = document.getElementById('checkbox-ul');
+    console.log(e.target.className)
+    if (e.target.className !== 'material-icons' && e.target.className !== 'cart-ul' && 
+      e.target.className !== 'material-icons remove-icon') {
+      cart.className === 'cart-ul' ? cart.className="hidden" : 0;
+    }
+    if (e.target.className !== 'sort-by') {
+      sortBy.className === 'sort-by-ul' ? sortBy.className = 'hidden' : 0;
+    }
+    if (e.target.className !== 'checkbox-ul' && e.target.className !== 'cbox' &&
+      e.target.className !== 'checkbox-div') {
+      filter.className === 'checkbox-ul' ? filter.className = 'hidden' : 0;
+    }
+
+    e.stopPropagation();
+  }
+
+  render() {
+    return(
+      <div onClick={this.toggleClass}>
+        <NavBarContainer />
+          {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default App;
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,50 +1,26 @@
 import React from 'react';
-import {fetchProducts} from '../actions/product_actions';
 import {Provider} from 'react-redux';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import App from './app';
+import {fetchProducts} from '../actions/product_actions';
 import ProductItemListContainer from './products/product_item_list_container';
-import NavBarContainer from './navbar/navbar_container';
-require('../../sass/reset.scss');
 
-class Root extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggleClass = this.toggleClass.bind(this);
+
+const Root = ({store}) => {
+
+  const fetchItems = () => {
+    store.dispatch(fetchProducts())
   }
 
-  componentDidMount() {
-    this.props.store.dispatch(fetchProducts());
-  }
-
-  toggleClass(e) {
-    let cart = document.getElementById('cart-ul');
-    let sortBy = document.getElementById('sort-by-ul');
-    let filter = document.getElementById('checkbox-ul');
-    console.log(e.target.className)
-    if (e.target.className !== 'material-icons' && e.target.className !== 'cart-ul' && 
-      e.target.className !== 'material-icons remove-icon') {
-      cart.className === 'cart-ul' ? cart.className="hidden" : 0;
-    }
-    if (e.target.className !== 'sort-by') {
-      sortBy.className === 'sort-by-ul' ? sortBy.className = 'hidden' : 0;
-    }
-    if (e.target.className !== 'checkbox-ul' && e.target.className !== 'cbox' &&
-      e.target.className !== 'checkbox-div') {
-      filter.className === 'checkbox-ul' ? filter.className = 'hidden' : 0;
-    }
-
-    e.stopPropagation();
-  }
-
-  render() {
-    return(
-      <Provider store={this.props.store}>
-        <div onClick={this.toggleClass}>
-          <NavBarContainer />
-          <ProductItemListContainer />
-        </div>
-      </Provider>
-    )
-  }
+  return (
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route path='/' component={App} onEnter={fetchItems}>
+          <IndexRoute component={ProductItemListContainer} />
+        </Route>
+      </Router>
+    </Provider>
+  )
 }
 
 export default Root;
